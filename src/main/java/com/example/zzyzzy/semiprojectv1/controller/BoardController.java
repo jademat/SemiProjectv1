db2925bae16a0ca0bd4cb0abd47db44eb12a19d6
jademat <01088049128a@gmail.com>
2025-02-27 10:03:26 +0900

    게시판 list 구현 7 - 페이지네이션 구현1

--- src/main/java/com/example/zzyzzy/semiprojectv1/controller/BoardController.java
+++ src/main/java/com/example/zzyzzy/semiprojectv1/controller/BoardController.java
@@ -22,6 +22,7 @@ public class BoardController {
         // RequestParam에 defaultValue 를 이용하면 cpg 매개변수가 전달되지 않을 경우 기본 값을 1 전달
         log.info("board/list 호출");
         m.addAttribute("bds", boardService.readBoard(cpg));
+        m.addAttribute("cpg", cpg);
         return "views/board/list";
     }
 
