61f1ff95c54d44d9b5ebdaff8be0dcd624cf6573
jademat <01088049128a@gmail.com>
2025-02-27 09:30:17 +0900

    게시판 list 구현 5 - stnum 계산을 위한 수식 작성 cpg에 대해 defaultValue 설정

--- src/main/java/com/example/zzyzzy/semiprojectv1/controller/BoardController.java
+++ src/main/java/com/example/zzyzzy/semiprojectv1/controller/BoardController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/board")
@@ -17,7 +18,8 @@ public class BoardController {
     private final BoardService boardService;
 
     @GetMapping("/list")
-    public String list(Model m,int cpg) {
+    public String list(Model m,@RequestParam(defaultValue = "1") int cpg) {
+        // RequestParam에 defaultValue 를 이용하면 cpg 매개변수가 전달되지 않을 경우 기본 값을 1 전달
         log.info("board/list 호출");
         m.addAttribute("bds", boardService.readBoard(cpg));
         return "views/board/list";
